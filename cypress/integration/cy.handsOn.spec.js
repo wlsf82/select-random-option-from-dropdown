ae08d045148f5ba222286789b926527317522721
Walmyr Filho <limaesilva@qelp.com>
2021-09-28 01:47:55 +0200

    Simplify things using cypress v8.5.0
    Now the .select function also accepts and index, not only a value.

--- cypress/integration/cy.handsOn.spec.js
+++ cypress/integration/cy.handsOn.spec.js
@@ -2,12 +2,7 @@ it('selects a random option from a select dropdown', () => {
   cy.visit('/index.html')
 
   cy.get('select option')
-    .as('options')
     .its('length', { log: false }).then(n => {
-      cy.get('@options', { log: false }).then($options => {
-        const randomOptionIndex = Cypress._.random(n - 1)
-        const randomOptionText = $options[randomOptionIndex].innerText
-        cy.get('select').select(randomOptionText)
-      })
+      cy.get('select').select(Cypress._.random(n - 1))
     })
 })
